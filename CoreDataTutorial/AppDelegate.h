fd0e43ffe0aa63b3e2b2a4ac53fe16c9812f85f8
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2015-01-09 13:57:49 -0500

    Initial commit of CoreDataTutorial project

---
+++ CoreDataTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataTutorial
+//
+//  Created by LuQuan Intrepid on 1/9/15.
+//  Copyright (c) 2015 LuQuan Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataTutorial
+//
+//  Created by LuQuan Intrepid on 1/9/15.
+//  Copyright (c) 2015 LuQuan Intrepid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
