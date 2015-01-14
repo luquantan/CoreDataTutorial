43b0317fb84b54b36e4192ad44f382b8a246dcc3
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2015-01-14 15:29:36 -0500

    Added methods to initialize the context and FlickrWebService at app launch. Kick off fetching from Flickr and background completion handlers.

--- CoreDataTutorial/AppDelegate.m
+++ CoreDataTutorial/AppDelegate.m
@@ -7,44 +7,58 @@
 //
 
 #import "AppDelegate.h"
+#import "FlickrWebService.h"
+#import "CDTSharedManagedDocument.h"
+
+static NSInteger const CDTForegroundFlickrFetchInterval = 20 * 60;
 
 @interface AppDelegate ()
-
+@property (strong, nonatomic) FlickrWebService *flickrWebService;
+@property (strong, nonatomic) NSManagedObjectContext *photoDatabaseContext;
 @end
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Should launch a flickr fetch right after the app launches.
+    //Call CDTSharedManagedDocument right at the start of the app so that when other parts of the app call the context, it will be available.
+    self.photoDatabaseContext = [CDTSharedManagedDocument sharedManagedDocument].context;
+    
+    [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
+    [self.flickrWebService startBackgroundFlickrFetch];
+    
     return YES;
 }
 
 - (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
-    // Perform foreground flickr fetch occasionally. An NSTimer and BackgrounfFetchInterval will be added.
-}
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [self.flickrWebService startForegroundFlickrFetchWithCompletion:completionHandler];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)application:(UIApplication *)application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)())completionHandler
+{
+    self.flickrWebService.flickrDownloadBackgroundURLSessionCompletionHandler = completionHandler;
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+#pragma mark - Initialize FlickrWebService
+- (FlickrWebService *)flickrWebService
+{
+    if (!_flickrWebService) {
+        _flickrWebService = [[FlickrWebService alloc] init];
+    }
+    return _flickrWebService;
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+#pragma mark - setter/getter for photoDatabaseContext
+- (void)setPhotoDatabaseContext:(NSManagedObjectContext *)photoDatabaseContext
+{
+    _photoDatabaseContext = photoDatabaseContext;
+    
+    [self.flickrWebService.flickrForegroundFetchTimer invalidate];
+    self.flickrWebService.flickrForegroundFetchTimer = nil;
+    
+    if (self.photoDatabaseContext) {
+        self.flickrWebService.flickrForegroundFetchTimer = [NSTimer scheduledTimerWithTimeInterval:CDTForegroundFlickrFetchInterval target:self.flickrWebService selector:@selector(startBackgroundFlickrFetch:) userInfo:nil repeats:YES];
+    }
 }
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 @end
